93af6c2426bb708b2b70df33d5397c3ab7e311bc
Robert Niederreiter <office@squarewave.at>
2013-02-21 15:20:37 +0100

    test section in setup

--- setup.py
+++ setup.py
@@ -36,6 +36,9 @@ setup(name="agx.generator.generator",
       ],
       extras_require=dict(
           ##code-section extras_require
+          test=[
+            'interlude',
+          ]
           ##/code-section extras_require
       ),
       entry_points="""
